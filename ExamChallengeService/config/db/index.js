2de6830bfa0cf790412679bdb9c68124849ceeb3
NguyenLamHoangAnh <just4codee@gmail.com>
2022-12-02 10:45:14 +0700

    done exam service

--- ExamChallengeService/config/db/index.js
+++ ExamChallengeService/config/db/index.js
@@ -1,8 +1,7 @@
 const mongoose = require("mongoose");
 async function connect() {
-  // await mongoose.connect(process.env.MONGO_URL_K8S,{
-  await mongoose
-    .connect(process.env.MONGO_URL_LOCAL, {
+  await mongoose.connect(process.env.MONGO_URL_K8S,{
+  // await mongoose.connect(process.env.MONGO_URL_LOCAL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     })
--- ExamService/Controllers/TopicController.cs
+++ ExamService/Controllers/TopicController.cs
@@ -30,23 +30,23 @@ namespace ExamService.Controllers
         [HttpPost]
         public IActionResult AddTopic(TopicRequestDto newTopic)
         {
-             ServiceResponse<TopicResponseDto> serviceResponse = _topicService.AddTopic(newTopic);
+            ServiceResponse<TopicResponseDto> serviceResponse = _topicService.AddTopic(newTopic);
             ControllerResponse<TopicResponseDto> controllerResponse = _mapper.Map<ControllerResponse<TopicResponseDto>>(serviceResponse);
             return StatusCode((int)serviceResponse.StatusCode, controllerResponse);
         }
 
-        [HttpPatch]
+        [HttpPatch("{oldTopicId}")]
         public IActionResult UpdateTopic(int oldTopicId, TopicRequestDto updateTopic)
         {
-             ServiceResponse<TopicResponseDto> serviceResponse = _topicService.UpdateTopic(oldTopicId, updateTopic);
+            ServiceResponse<TopicResponseDto> serviceResponse = _topicService.UpdateTopic(oldTopicId, updateTopic);
             ControllerResponse<TopicResponseDto> controllerResponse = _mapper.Map<ControllerResponse<TopicResponseDto>>(serviceResponse);
             return StatusCode((int)serviceResponse.StatusCode, controllerResponse);
         }
 
-        [HttpDelete]
-        public IActionResult UpdateTopic(int oldTopicId)
+        [HttpDelete("{topicToDeleteId}")]
+        public IActionResult UpdateTopic(int topicToDeleteId)
         {
-             ServiceResponse<TopicResponseDto> serviceResponse = _topicService.DeleteTopic(oldTopicId);
+            ServiceResponse<TopicResponseDto> serviceResponse = _topicService.DeleteTopic(topicToDeleteId);
             ControllerResponse<TopicResponseDto> controllerResponse = _mapper.Map<ControllerResponse<TopicResponseDto>>(serviceResponse);
             return StatusCode((int)serviceResponse.StatusCode, controllerResponse);
         }
